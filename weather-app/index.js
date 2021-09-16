c3008102922cf5e9dbac2849772006ad428dffb3
yoannakr <jkrmilkova@gmail.com>
2021-09-16 17:53:37 +0300

    add dynamic icons

--- weather-app/index.js
+++ weather-app/index.js
@@ -28,7 +28,6 @@ const weather = {
   temperatureUnit: temperatureUnits.CELSIUS,
   description: undefined,
   location: undefined,
-  icon: "unknown",
 };
 
 function kelvinToCelsius(temperature) {
@@ -63,7 +62,6 @@ function displayCurrentWeather() {
   temperatureUnitElement.textContent = weather.temperatureUnit;
   descriptionElement.textContent = weather.description;
   locationElement.textContent = weather.location;
-  iconElement.src = `public/icons/${weather.icon}.png`;
 }
 
 function getCurrentWeather(latitude, longitude) {
@@ -74,7 +72,7 @@ function getCurrentWeather(latitude, longitude) {
       weather.temperature = kelvinToCelsius(data.main.temp);
       weather.description = data.weather[0].description;
       weather.location = `${data.name}, ${data.sys.country}`;
-      weather.icon = data.weather[0].icon;
+      setImage(data);
 
       displayCurrentWeather();
     });
@@ -140,3 +138,43 @@ function getCityInformation(cityName) {
 searchButtonElement.addEventListener("click", () =>
   getCityInformation(cityNameInputElement.value)
 );
+
+setImage = (data) => {
+  if (data.weather[0].main === "Clear") {
+    iconElement.src = "public/images/sun.svg";
+  }
+
+  if (data.weather[0].main === "Snow") {
+    iconElement.src = "public/images/snow.svg";
+  }
+
+  if (data.weather[0].main === "Thunderstorm") {
+    iconElement.src = "public/images/thunderstorm.svg";
+  }
+
+  if (
+    data.weather[0].main === "Drizzle" ||
+    data.weather[0].main === "Mist" ||
+    data.weather[0].main === "Smoke" ||
+    data.weather[0].main === "Haze" ||
+    data.weather[0].main === "Dust" ||
+    data.weather[0].main === "Fog" ||
+    data.weather[0].main === "Sand" ||
+    data.weather[0].main === "Dust" ||
+    data.weather[0].main === "Ash" ||
+    data.weather[0].main === "Squall" ||
+    data.weather[0].main === "Tornado"
+  ) {
+    iconElement.src = "public/images/drizzle.svg";
+  }
+
+  if (data.weather[0].main === "Clouds") {
+    if (data.weather[0].description === "few clouds")
+      iconElement.src = "public/images/few_clouds.svg";
+    else iconElement.src = "public/images/overcast_clouds.svg";
+  }
+
+  if (data.weather[0].main === "Rain") {
+    iconElement.src = "public/images/light_rain.svg";
+  }
+};
